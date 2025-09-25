d9b94f6ce2da912e0e186c83ac341f276a8de0f1
bukyong <70992081+bukyong@users.noreply.github.com>
2025-09-25 22:31:25 +0900

    first release

---
+++ UpbitTradingBot/src/main/java/com/utb/UpbitTradingBotApplication.java
@@ -0,0 +1,13 @@
+package com.utb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpbitTradingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpbitTradingBotApplication.class, args);
+	}
+
+}
---
+++ UpbitTradingBot/src/test/java/com/utb/UpbitTradingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpbitTradingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
